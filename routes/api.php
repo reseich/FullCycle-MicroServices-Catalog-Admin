9009fa1dcb7c067de47e0d56010592716a055723
Rafael Eich da Silva <reseich@gmail.com>
2021-03-14 11:07:39 -0400

    castMembers and abstractCrud

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::resource('categories', 'CategoryController', ['except'=>['create','edit']]);
 Route::resource('genres', 'GenreController', ['except'=>['create','edit']]);
+Route::resource('cast_members', 'CastMemberController', ['except'=>['create','edit']]);
