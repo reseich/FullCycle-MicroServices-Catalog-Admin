854d265b7fd265c5b763fdffb3da3d9040116084
Rafael Eich da Silva <reseich@gmail.com>
2021-04-17 09:55:03 -0400

    change project to typescript

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
-npm config set cache /var/www/.npm-cache --global
-cd /var/www/frontend && npm install && cd ..
 cd backend
 cp .env.example .env
 cp .env.testing.example .env.testing
