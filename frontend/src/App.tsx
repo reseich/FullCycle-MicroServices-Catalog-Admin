55c20ca91061e507f1b0ad1276597b92057e36d4
Rafael Eich da Silva <reseich@gmail.com>
2021-06-19 16:22:20 -0400

    production build

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ const App: React.FC = () => {
                 <MuiThemeProvider theme={theme}>
                     <SnackbarProvider>
                         <CssBaseline/>
-                        <BrowserRouter>
+                        <BrowserRouter basename={'/admin'}>
                             <Spinner/>
                             <Navbar/>
                             <Box paddingTop={'70px'}>
